20c024bea95c26193e52dce6d807ead2766e3b00
jkomyno <skiabo97@gmail.com>
2022-02-12 02:18:03 +0100

    feat(range): adapted range from private repo

---
+++ include/jkds/util/range.h
@@ -0,0 +1,19 @@
+#include <concepts>
+#include <numeric>
+#include <vector>
+
+namespace jkds::util {
+
+  /***
+   * range 
+   * 
+   * Utility that generates a sequential range of values of a specific size.
+   * If the start value is not specified, the range starts from 0.
+   */
+  template<typename T>
+  auto range(std::size_t size, T start = T(0)) -> std::vector<T> {
+    std::vector<T> vec(size);
+    std::iota(vec.begin(), vec.end(), start);
+    return vec;
+  }
+} // namespace jkds::util
